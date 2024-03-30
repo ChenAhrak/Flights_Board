6bb8cad19b2c842bf91faa88ce4bbf253cc33216
ChenAhrak <chenahrak@gmail.com>
2024-03-30 15:06:57 +0300

    dynamic to static and filter table function not finish

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 ﻿
 
-
-
-const header = document.createElement("h1");
-header.innerHTML = "ברוכים הבאים ללוח הטיסות הכי גדול במדינה";
-document.body.insertBefore(header, document.body.firstChild); // Insert at the beginning of the body
-
 const searchContainer = document.getElementById("searchContainer");
-const searchForm = document.createElement("form")
 
-searchForm.innerHTML = `
-    <input type="radio" name="typeOfFlight" id="departures">
-    <label for="departures">המראות</label><br>
-    <input type="radio" name="typeOfFlight" id="arrivals">
-    <label for="arrivals">נחיתות</label><br>
-    <input type="text" placeholder="מספר טיסה"><br>
-    <select name="country" id="country"></select><br>
-    <select name="city" id="city"></select><br>
-    <input type="date" id="from" placeholder="מתאריך"><br>
-    <input type="date" id="to" placeholder="עד תאריך"><br>
-    <button>חיפוש</button>`;
-
-
-searchContainer.appendChild(searchForm);
 
 // Extract unique country values from jsonFlights
 const uniqueCountries = new Set(" "); // Initialize with an empty string to add an empty option
@@ -61,23 +40,9 @@ uniqueCities.forEach(city => {
 });
 
 const tableContainer = document.getElementById("tableContainer");
-const table = document.createElement("table");
-
-// Create the table header row
-const headerRow = document.createElement("tr");
-const searchInput = document.createElement("input");
-
-headerRow.innerHTML = "<td>המראות</td><td>נחיתות</td>"
-tableContainer.appendChild(table);
-table.appendChild(headerRow);
-
-const bodyRows = document.createElement("tr");
-bodyRows.innerHTML = "<td>סוג טיסה</td><td>מספר הטיסה</td><td>שם מלא של חברת התעופה</td><td>זמן מתוכנן</td>" +
-    "<td>זמן בפועל</td><td>שם שדה התעופה ביעד / במקור</td>" +
-    "<td>שם העיר ביעד / במקור</td><td>ארץ היעד / המקור</td>" +
-    "<td>טרמינל</td><td>סטטוס</td>"
-
-table.appendChild(bodyRows);
+const table = document.getElementById("flights-table");
+const tableBody = document.createElement("tbody");
+tableBody.id = "table-body";
 
 function populateTable() {
     for (let item in jsonFlights) {
@@ -95,15 +60,60 @@ function populateTable() {
             `<td>${jsonFlights[item].schedueTime}</td><td>${jsonFlights[item].actualTime}</td>` +
             `<td>${jsonFlights[item].airport}</td><td>${jsonFlights[item].city}</td>` +
             `<td>${jsonFlights[item].country}</td><td>${jsonFlights[item].terminal}</td><td>${jsonFlights[item].status}</td>`;
-        table.appendChild(bodyRow)
+        tableBody.appendChild(bodyRow)
         if (jsonFlights[item].type == "A") bodyRow.style.backgroundColor = "#afeeee";
         else {
             bodyRow.style.backgroundColor = "#9cffed";
         }
     }
+    table.appendChild(tableBody);
     tableContainer.appendChild(table);
 }
 populateTable();
+
+function filterTable() {
+
+    const flightType = document.getElementsByName("flight-type");
+
+    const country = document.getElementById("country").value.trim();
+    const city = document.getElementById("city").value.trim();
+    const from = document.getElementById("from").value.trim();
+    const to = document.getElementById("to").value.trim();
+    console.log(country, city, from, to, flightType[0].checked, flightType[1].checked);
+    if (country == "" && city == "" && from == "" && to == "" && !flightType[0].checked && !flightType[1].checked) {
+        alert("Please select a filter");
+    }
+    else {
+        tableBody.innerHTML = "";
+        for (let item in jsonFlights) {
+            if (country == jsonFlights[item].country) {
+                var bodyRow = document.createElement("tr");
+                const image = document.createElement("img");
+                image.src = jsonFlights[item].type === "A" ? "arrival.png" : "departure.png";
+                image.style.display = "block";
+                image.style.margin = "0 auto";
+                image.style.width = "25px";
+                image.style.height = "25px";
+                const firstCell = document.createElement("td");
+                firstCell.appendChild(image);
+                bodyRow.appendChild(firstCell);
+                bodyRow.innerHTML += `<td>${jsonFlights[item].number}</td><td>${jsonFlights[item].operatorLong}</td>` +
+                    `<td>${jsonFlights[item].schedueTime}</td><td>${jsonFlights[item].actualTime}</td>` +
+                    `<td>${jsonFlights[item].airport}</td><td>${jsonFlights[item].city}</td>` +
+                    `<td>${jsonFlights[item].country}</td><td>${jsonFlights[item].terminal}</td><td>${jsonFlights[item].status}</td>`;
+                tableBody.appendChild(bodyRow)
+                if (jsonFlights[item].type == "A") bodyRow.style.backgroundColor = "#afeeee";
+                else {
+                    bodyRow.style.backgroundColor = "#9cffed";
+                }
+
+            }
+
+        }
+    }
+}
+
+
 
 const tableRows = document.querySelectorAll("tr");
 const lightBlue = "#89CFF0";
